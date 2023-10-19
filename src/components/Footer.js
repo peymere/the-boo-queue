93b0a73cd73c09037d9d5c64aae47a980fa448a3
Peyton Meredith <peymeredith@gmail.com>
2023-10-19 15:31:03 -0500

    new fonts

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Footer() {
+    return (
+        <footer className="footer-container">
+            <h2>footer</h2>
+        </footer>
+    );
+}
+
+export default Footer; 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,29 +6,29 @@ function NavBar() {
         <nav className="navbar navbar-expand-lg navbar-light fixed-top custom-navbar">
         <ul className="nav-links">
           <li>
-        <NavLink 
-        to= "/movies"
-        className="nav-link" 
-        >
-          Movies
-        </NavLink>
-        </li>
-        <li>
-        <NavLink 
-        to= "/about"
-        className="nav-link" 
-        >
-           About
-        </NavLink>
-        </li>
-        <li>
-        <NavLink 
-        to= "/addmovie"
-        className="nav-link" 
-        >
-          Add a movie!
-        </NavLink>
-        </li>
+            <NavLink 
+            to= "/movies"
+            className="nav-link" 
+            >
+              Movies
+            </NavLink>
+          </li>
+          <li>
+            <NavLink 
+            to= "/about"
+            className="nav-link" 
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink 
+            to= "/addmovie"
+            className="nav-link" 
+            >
+            Add a movie!
+            </NavLink>
+          </li>
         </ul>
       </nav>   
     );
