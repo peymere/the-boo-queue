b1c70022dfb9400017978a26c1e78032a6b00f4a
Peyton Meredith <peymeredith@gmail.com>
2023-10-19 15:46:09 -0500

    populated about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,18 @@ import React from "react";
 
 function About() {
     return (
-        <h1>About</h1>
+        <div className="about-container">
+            <h1 className="about-title">About</h1>
+            <p className="about-text">
+                Welcome to the Boo Queue, your ultimate destination for all things spooky and spine-chilling! We're passionate about Halloween and everything that goes bump in the night, and we've created this site to share our love for the season of scares. 
+            </p>
+            <p className="about-text">
+                Our team of dedicated horror enthusiasts has carefully curated a selection of the best Halloween movies that are perfect for your eerie evenings. Whether you're a fan of classic slasher flicks, supernatural thrillers, or family-friendly spooky films, we've got you covered. You'll find a mix of timeless classics and hidden gems, each with its own frightful charm.
+            </p>
+            <p className="about-text">
+                So, grab your popcorn, dim the lights, and join us on a hair-raising journey through the world of Halloween cinema. Whether you're seeking to revisit your favorite ghoulish classics or discover new blood-curdling tales, the Boo Queue is here to be your trusted companion in the dark. Let's make this Halloween season one to remember!
+            </p>
+        </div>
     );
 }
 
